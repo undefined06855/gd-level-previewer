ce96ef414c01dd8476d6e292fe427f037f7c6f2e
undefined06855 <currentcashew57@gmail.com>
2023-10-15 13:14:58 +0100

    host on public server

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function getGDLevel(id)
 {
-    fetch("http://127.0.0.1:5000/get/" + id)
+    fetch("http://82.26.119.92:25565/get/" + id)
     .then(data => data.text())
     .then(data => {
         console.log(JSON.parse(data))
